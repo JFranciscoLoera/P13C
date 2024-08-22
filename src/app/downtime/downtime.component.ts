94ae0619c649c9135ecb67524fa1dd635b1eab05
JFranciscoLoera <francisco182008@live.com.mx>
2024-08-22 08:30:54 -0600

    Creacion downtown component

---
+++ src/app/downtime/downtime.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-downtime',
+  standalone: true,
+  imports: [],
+  templateUrl: './downtime.component.html',
+  styleUrl: './downtime.component.css'
+})
+export class DowntimeComponent {
+
+}
