0ac3f9c5103d03dd207b4b7e8b2e8eedf8d20010
JFranciscoLoera <francisco182008@live.com.mx>
2024-08-23 09:24:22 -0600

    Router outlet configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { DowntimeComponent } from './downtime/downtime.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', component: DowntimeComponent },
+  { path: 'downtime', component: DowntimeComponent }
+];
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { importProvidersFrom } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { provideRouter, RouterOutlet } from '@angular/router';
+import { routes } from './app/app.routes';
 
-bootstrapApplication(AppComponent,{
-  providers:[importProvidersFrom([BrowserAnimationsModule])]
-});
+// Configura y arranca la aplicación Angular
+bootstrapApplication(AppComponent, {
+  providers: [
+    // Proporciona BrowserAnimationsModule y el enrutador
+    importProvidersFrom(BrowserAnimationsModule),
+    provideRouter(routes) // Proporciona el enrutador con las rutas definidas
+  ]
+})
+.catch(err => console.error(err));
