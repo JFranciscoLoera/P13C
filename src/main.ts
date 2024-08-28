f3f51b8d12054026f97b4d5841438ea00735ebab
JFranciscoLoera <francisco182008@live.com.mx>
2024-08-28 17:32:49 -0600

    Justo despues de instalar Angular Material

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 bootstrapApplication(AppComponent, {
   providers: [
     importProvidersFrom(BrowserAnimationsModule, ReactiveFormsModule,HttpClientModule),
-    provideRouter(routes)
+    provideRouter(routes), provideAnimationsAsync()
   ]
 })
 .catch(err => console.error(err));
