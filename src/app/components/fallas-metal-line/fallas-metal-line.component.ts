b3d2cc304f2b4dd15019d4c800586342abf83b6d
JFranciscoLoera <francisco182008@live.com.mx>
2024-08-28 13:10:00 -0600

    Creacion del componente Fallas Metal Line

---
+++ src/app/components/fallas-metal-line/fallas-metal-line.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { SearchBarComponent } from '../../shared/search-bar/search-bar.component';
+
+@Component({
+  selector: 'app-fallas-metal-line',
+  standalone: true,
+  imports: [SearchBarComponent],
+  templateUrl: './fallas-metal-line.component.html',
+  styleUrl: './fallas-metal-line.component.css'
+})
+export class FallasMetalLineComponent {
+
+}
